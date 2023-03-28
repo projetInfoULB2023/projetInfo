eef3ca431e2baa883dbb5d3fc22a1adf5b7344a6
projetInfoULB2023 <projetinfoulb2023@gmail.com>
2023-03-28 02:29:46 +0200

    Upload projet initial

---
+++ app/src/main/java/com/example/jeuinfo/DrawingView.kt
@@ -0,0 +1,77 @@
+package com.example.jeuinfo
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import java.util.*
+
+class DrawingView @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0): SurfaceView(context, attributes,defStyleAttr),
+    SurfaceHolder.Callback,Runnable {
+    private val backgroundPaint = Paint()
+    private val random = Random()
+    private var drawing = true
+    lateinit var canvas:Canvas
+    lateinit var thread:Thread
+
+    private fun draw(){
+        if(holder.surface.isValid){
+            canvas =holder.lockCanvas()
+            backgroundPaint.color= Color.WHITE
+            canvas?.drawRect(0F,0F,width.toFloat(),height.toFloat(),backgroundPaint)
+            //Code pour dessiner ici
+            holder.unlockCanvasAndPost(canvas)
+
+        }
+    }
+
+    fun pause(){
+        drawing = false
+        thread.join()
+    }
+
+    fun resume(){
+        drawing = true
+        thread=Thread(this)
+        thread.start()
+    }
+
+    override fun onTouchEvent(e: MotionEvent): Boolean {
+        //S'active quand l'écran est touché
+        if (e.action == MotionEvent.ACTION_DOWN) {
+            // x et y donnent la position du click, il faudrait encore tester le y par rapport à la position de notre drawingview sur l'écran
+            val x = e.rawX
+            val y = e.rawY - 800
+
+            println("($x,$y)")
+        }
+        //invalidate permet de dessiner ce qui a été changé
+        invalidate()
+
+        return true
+    }
+
+    override fun run() {
+        while(drawing){
+            draw()
+        }
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ app/src/main/java/com/example/jeuinfo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.jeuinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jeuinfo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
