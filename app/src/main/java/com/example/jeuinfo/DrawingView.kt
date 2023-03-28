82995d240b2f0ed27cd3f342a79ddcce4779c6f4
projetInfoULB2023 <projetinfoulb2023@gmail.com>
2023-03-28 16:01:38 +0200

    explication commit

--- app/src/main/java/com/example/jeuinfo/DrawingView.kt
+++ app/src/main/java/com/example/jeuinfo/DrawingView.kt
@@ -17,7 +17,7 @@ class DrawingView @JvmOverloads constructor (context: Context, attributes: Attri
     private var drawing = true
     lateinit var canvas:Canvas
     lateinit var thread:Thread
-    var test = 5
+    var test = 8
     private fun draw(){
         if(holder.surface.isValid){
             canvas =holder.lockCanvas()
