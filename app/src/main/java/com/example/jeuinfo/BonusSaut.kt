1df83b3746a9e09306fd86f502945247ef787a28
projetInfoULB2023 <projetinfoulb2023@gmail.com>
2023-05-06 01:30:35 +0200

    Classes Bonus commentées

--- app/src/main/java/com/example/jeuinfo/BonusSaut.kt
+++ app/src/main/java/com/example/jeuinfo/BonusSaut.kt
@@ -4,13 +4,9 @@ import android.graphics.Bitmap
 import android.graphics.RectF
 import kotlin.math.abs
 
-class BonusSaut(x1:Float,
-               y1:Float,
-               largeur:Float,
-               hauteur:Float,
-               vitesseX:Float,
-               vitesseY:Float,
-               image: Bitmap) : Bonus(x1,y1, largeur, hauteur, vitesseX, vitesseY, image),CollisionDisparition {
+class BonusSaut(x1:Float, y1:Float, largeur:Float, hauteur:Float, vitesseX:Float, vitesseY:Float, image: Bitmap) : Bonus(x1, y1, largeur, hauteur, vitesseX, vitesseY, image), CollisionDisparition {
+
+    //réécriture de la fonction collision() pour le cas d'une collision avec un bonus de saut
     override fun collision(joueur:Joueur) {
         joueur.r = RectF(joueur.x1+marge,joueur.y1+marge,joueur.x1+joueur.largeur-marge,joueur.y1+joueur.hauteur-marge)
         if(abs(this.y1-joueur.y1) < joueur.taillejoueur*2){
--- app/src/main/java/com/example/jeuinfo/BonusVie.kt
+++ app/src/main/java/com/example/jeuinfo/BonusVie.kt
@@ -4,20 +4,16 @@ import android.graphics.Bitmap
 import android.graphics.RectF
 import kotlin.math.abs
 
-class BonusVie(x1:Float,
-               y1:Float,
-               largeur:Float,
-               hauteur:Float,
-               vitesseX:Float,
-               vitesseY:Float,
-               image: Bitmap) : Bonus(x1,y1, largeur, hauteur, vitesseX, vitesseY, image),CollisionDisparition {
+class BonusVie(x1:Float, y1:Float, largeur:Float, hauteur:Float, vitesseX:Float, vitesseY:Float, image: Bitmap) : Bonus(x1, y1, largeur, hauteur, vitesseX, vitesseY, image), CollisionDisparition {
+
+    //réécriture de la fonction collision() pour le cas d'une collision avec un bonus de vie
     override fun collision(joueur:Joueur) {
         joueur.r = RectF(joueur.x1+marge,joueur.y1+marge,joueur.x1+joueur.largeur-marge,joueur.y1+joueur.hauteur-marge)
         if(abs(this.y1-joueur.y1) < joueur.taillejoueur*2){
             if(this.r.intersect(joueur.r)){
                 joueur.bonuses.add(this)
                 DrawingView.toBeRemoved.add(this)
-                joueur.lives+=1
+                joueur.lives += 1
             }
         }
     }
